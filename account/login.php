b2412f7d4e26c32928f16381bb804301f4d030f2
kajadhav <kajadhav@ncsu.edu>
2023-12-31 04:14:55 -0500

    Minor fixes and hosting

--- account/login.php
+++ account/login.php
@@ -51,6 +51,35 @@ else{
 
 
   <div id="login-page" class="row">
+  <header id="main-navigation">
+  <div id="navigation" data-spy="affix" data-offset-top="20">
+    <div class="container">
+      <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-default">
+          <!-- <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#fixed-collapse-navbar" aria-expanded="false"> 
+            <span class="icon-bar top-bar"></span> <span class="icon-bar middle-bar"></span> <span class="icon-bar bottom-bar"></span> 
+            </button>
+           <a class="navbar-brand" href="index.html"><img src="../images/logo.png" alt="logo" class="img-responsive"></a> 
+         </div> -->
+        
+            <div id="fixed-collapse-navbar" class="navbar-collapse collapse navbar-right">
+              <ul class="nav navbar-nav">
+                <li>
+                   <a href="../index.html">Home</a>
+                </li>
+                <li><a href="../food.html">Our Food</a></li>
+                <li><a href="../about.html">About</a></li>
+                <li><a href="../faq.html">FAQ</a></li>
+              </ul>
+            </div>
+         </nav>
+         </div>
+       </div>
+     </div>
+  </div>
+</header>
     <div class="col s12 z-depth-4 card-panel">
       <form method="post" action="routers/router.php" class="login-form" id="form">
         <div class="row">
