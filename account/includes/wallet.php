7f1b09d673a91e34c1a5cfaf5d3c27022998a6ce
kajadhav <kajadhav@ncsu.edu>
2024-01-01 01:52:12 -0500

    Fix: wallet card number

--- account/includes/wallet.php
+++ account/includes/wallet.php
@@ -1,11 +1,9 @@
 <?php
 $user_id=$_SESSION['user_id'];
-$sql = mysqli_query($con, "SELECT * FROM wallet where customer_id = $user_id");
-while($row1 = mysqli_fetch_array($sql)){
+$sql = $con->query("SELECT * FROM wallet where customer_id = $user_id");
+$row1 = $sql->fetch_assoc();
 $wallet_id = $row1['id'];
-}
-$sql = mysqli_query($con, "SELECT * FROM wallet_details where wallet_id = $wallet_id");
-while($row1 = mysqli_fetch_array($sql)){
+$sql = $con->query("SELECT * FROM wallet_details where wallet_id = $wallet_id");
+$row1 = $sql->fetch_assoc();
 $balance = $row1['balance'];
-}
 ?>
